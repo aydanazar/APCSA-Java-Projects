cf23f1a3032c4747b445c439c891b70f82e4df23
Ayda Nayeb Nazar <ayda.nazar@gmail.com>
2019-02-01 12:39:04 -0800

    added unit00 and unit01

---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,34 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Ayda Nayeb Nazar \t  01/31/19 \n\n" );
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+
+	}
+}
