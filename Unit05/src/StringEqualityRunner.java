fe1e9dd5fe5bc6d28aa811cac8c2c44561517adb
Ayda <ayda.nayebnazar@yahoo.ca>
2019-02-08 13:25:09 -0800

    finished unit05 assignments

--- Unit05/src/StringEqualityRunner.java
+++ Unit05/src/StringEqualityRunner.java
@@ -12,7 +12,39 @@ public class StringEqualityRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases
+		StringEquality test = new StringEquality();
+		
+		test.setWords("hello", "goodbye");
+		test.checkEquality();
+		System.out.println(test);
+		
+		test.setWords("one", "two");
+		test.checkEquality();
+		System.out.println(test);
+		
+		test.setWords("three", "four");
+		test.checkEquality();
+		System.out.println(test);
+		
+		test.setWords("TCEA", "UIL");
+		test.checkEquality();
+		System.out.println(test);
+		
+		test.setWords("State", "Champions");
+		test.checkEquality();
+		System.out.println(test);
+		
+		test.setWords("ABC", "ABC");
+		test.checkEquality();
+		System.out.println(test);
+		
+		test.setWords("ABC", "CBA");
+		test.checkEquality();
+		System.out.println(test);
+		
+		test.setWords("Same", "Same");
+		test.checkEquality();
+		System.out.println(test);
 		
 	}
 }
--- Unit05/src/WordsCompareRunner.java
+++ Unit05/src/WordsCompareRunner.java
@@ -12,7 +12,38 @@ public class WordsCompareRunner
 {
    public static void main( String args[] )
    {
-		//add test cases
-
+		WordsCompare test = new WordsCompare();
+		
+		test.setWords("abe", "ape");
+		test.compare();
+		System.out.println(test);
+		
+		test.setWords("giraffe", "gorilla");
+		test.compare();
+		System.out.println(test);
+		
+		test.setWords("one", "two");
+		test.compare();
+		System.out.println(test);
+		
+		test.setWords("fun", "funny");
+		test.compare();
+		System.out.println(test);
+		
+		test.setWords("123", "19");
+		test.compare();
+		System.out.println(test);
+		
+		test.setWords("193", "1910");
+		test.compare();
+		System.out.println(test);
+		
+		test.setWords("goofy", "godfather");
+		test.compare();
+		System.out.println(test);
+		
+		test.setWords("funnel", "fun");
+		test.compare();
+		System.out.println(test);
 	}
 }
