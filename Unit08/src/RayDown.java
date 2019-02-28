3dfceff7b0e272d15def1aa2e3a0896372e4e003
Ayda <ayda.nayebnazar@yahoo.ca>
2019-02-27 19:27:07 -0800

    finished unit08

--- Unit08/src/RayDown.java
+++ Unit08/src/RayDown.java
@@ -7,8 +7,15 @@ public class RayDown
 {
 	//go() will return true if all numbers in numArray
 	//are in decreasing order [31,12,6,2,1]
-	public static boolean go(int[] numArray)
+	public boolean go(int[] numArray)
 	{
-		return true;
-	}	
+		boolean isDecreasing = true;
+		for (int i = 0; i < numArray.length-1; i++) {
+			if (numArray[i] <= numArray[i+1]) {
+				isDecreasing = false;
+				break;
+			}
+		}
+		return isDecreasing;
+	}
 }
--- Unit08/src/RayOddToEven.java
+++ Unit08/src/RayOddToEven.java
@@ -5,8 +5,27 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public int go(int[] ray)
 	{
-		return false;
+		int space = 0;
+		boolean isOdd = false;
+		boolean isEven = false;
+		for (int i = 0; i < ray.length; i++) {
+			if (ray[i] % 2 == 1) {
+				isOdd = true;
+				for (int n = i; n < ray.length; n++) {
+					if (ray[n] % 2 == 0) {
+						isEven = true;
+						space = n - i;
+						break;
+					}
+				}
+				break;
+			}
+		}
+		if (isOdd && isEven) {
+			return space;
+		}
+		return -1;
 	}
 }
